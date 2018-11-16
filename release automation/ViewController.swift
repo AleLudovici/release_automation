12eebd9a2aef959f3e26c34e3162a9e7b898032d
AleLudovici <alessandro.ludovici@gmail.com>
2018-11-16 12:42:06 +0000

    Remove error

--- release automation/ViewController.swift
+++ release automation/ViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let a: Int
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
