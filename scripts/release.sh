e73414aa4d17c5ad5ee0f15da69f8ad3adf4e4ee
AleLudovici <alessandro.ludovici@gmail.com>
2018-11-16 12:10:15 +0000

    Split script into step. Ensure brach is in a clean state.

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,8 +2,29 @@
 
 set -e
 
-git checkout master &&
-git pull &&
-git checkout release &&
-git merge master &&
-git push origin release
+function merge_master_to_release() {
+	git checkout master &&
+	git pull &&
+	git checkout release &&
+	git merge master
+}
+
+function build() {
+	echo 'building...'
+}
+
+function push() {
+	git push origin release
+}
+
+if [ -z "$(git status --porcelain)" ]; then 
+  # Working directory clean
+  merge_master_to_release
+  build
+  push
+else 
+  # Uncommitted changes
+  echo 'You have uncommitted changes. Please commit them before releasing.'
+fi
+
+
