d8234c63bacf098886c6ab4a3804579fdc683f98
Alessandro Ludovici <alessandro.ludovici@gmail.com>
2018-12-03 14:06:30 +0000

    fake commit for a feature

--- release automation/ViewController.swift
+++ release automation/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
 
         // simple change to open PR
+        let f = "a feature"
     }
 
 
